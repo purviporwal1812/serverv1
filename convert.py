c518e53f12a1f6707bbf479b08572dd55593512b
Purvi Porwal <purviporwal1812@gmail.com>
2025-04-05 18:24:03 +0530

    converting keras file

---
+++ convert.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+
+# Load your trained Keras model
+model = tf.keras.models.load_model('models/final_model.keras')  # or .keras
+
+# Convert the model to TensorFlow Lite format
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+tflite_model = converter.convert()
+
+# Save the converted model to disk
+with open('models/final_model.tflite', 'wb') as f:
+    f.write(tflite_model)
+
+print("✅ Conversion to TFLite complete!")
